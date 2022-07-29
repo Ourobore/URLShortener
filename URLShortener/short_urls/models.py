62037511baf5c0171badf0116fe7e46a6afd6010
Ourobore <lena.chaprenet@gmail.com>
2022-07-29 08:35:43 +0000

    Made migrations, can now generate and store short urls. Setuped admin panel

--- URLShortener/short_urls/models.py
+++ URLShortener/short_urls/models.py
@@ -7,8 +7,12 @@ SHORT_URL_CHARSET = string.ascii_letters
 
 # Create your models here.
 class ShortUrl(models.Model):
-    original_url = models.URLField()
-    short_url = models.URLField()
 
-    def generate_random_short_url():
+    slug = models.URLField()
+    original_url = models.URLField()
+
+    def __str__(self):
+        return self.slug
+
+    def generate_random_short_slug():
         return ''.join(random.choices(SHORT_URL_CHARSET, k=SHORT_URL_LENGTH))
--- URLShortener/short_urls/views.py
+++ URLShortener/short_urls/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .models import ShortUrl
 
 # Create your views here.
 def index(request):
     return render(request, 'short_urls/index.html')
 
 def url_form(request):
+    # Creating new ShortUrl
+    new_short_url = ShortUrl(
+        original_url=request.POST['input_url'],
+        slug=ShortUrl.generate_random_short_slug(),
+    )
+    new_short_url.save()
     return HttpResponseRedirect(reverse('short_urls:index'))
