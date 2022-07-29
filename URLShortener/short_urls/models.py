c4349e0ea74040ff7882b20424a5020aabfac332
Ourobore <lena.chaprenet@gmail.com>
2022-07-29 08:13:22 +0000

    Added ShortUrl model, with method to generate random slug

--- URLShortener/short_urls/models.py
+++ URLShortener/short_urls/models.py
@@ -1,3 +1,14 @@
+import string
+import random
 from django.db import models
 
+SHORT_URL_LENGTH = 6
+SHORT_URL_CHARSET = string.ascii_letters
+
 # Create your models here.
+class ShortUrl(models.Model):
+    original_url = models.URLField()
+    short_url = models.URLField()
+
+    def generate_random_short_url():
+        return ''.join(random.choices(SHORT_URL_CHARSET, k=SHORT_URL_LENGTH))
