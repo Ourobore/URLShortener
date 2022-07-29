998407fb94c4fb7678c5218bf3f3d0471af3c4eb
Ourobore <lena.chaprenet@gmail.com>
2022-07-29 07:57:33 +0000

    Redirection s working after form submission

--- URLShortener/short_urls/urls.py
+++ URLShortener/short_urls/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'short_urls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('url_form', views.url_form, name='url_form'),
 ]
--- URLShortener/short_urls/views.py
+++ URLShortener/short_urls/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'short_urls/index.html')
+
+def url_form(request):
+    return HttpResponseRedirect(reverse('short_urls:index'))
