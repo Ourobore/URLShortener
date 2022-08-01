dad17d7e0f5c808ac155f7a286437332751bca8b
Ourobore <lena.chaprenet@gmail.com>
2022-08-01 06:58:20 +0000

    Rolled back to intermediate view and redirection

--- URLShortener/short_urls/urls.py
+++ URLShortener/short_urls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'short_urls'
+app_name = "short_urls"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:slug>', views.redirect_short_url, name='redirect_short_url')
+    path("", views.index, name="index"),
+    path("url_form", views.url_form, name="url_form"),
+    path("<str:slug>", views.redirect_short_url, name="redirect_short_url"),
 ]
--- URLShortener/short_urls/views.py
+++ URLShortener/short_urls/views.py
@@ -1,30 +1,41 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.conf import settings
 from .models import ShortUrl, ShortUrlForm
 
 # Create your views here.
 def index(request):
     # Get previous slug
-    print(request.POST)
-    if slug := request.session.get('slug', None):
-        del request.session['slug']
+    if slug := request.session.get("slug", None):
+        del request.session["slug"]
+    if not_valid := request.session.get("not_valid", None):
+        del request.session["not_valid"]
 
-    if request.method == 'POST':
+    return render(
+        request,
+        "short_urls/index.html",
+        {
+            "formset": ShortUrlForm(),
+            "slug": slug,
+            "not_valid": not_valid,
+            "host": request.get_host(),
+        },
+    )
+
+
+def url_form(request):
+    if request.method == "POST":
         # Process the form
         formset = ShortUrlForm(request.POST)
         if formset.is_valid():
-            print('is_valid()')
             new_short_url = formset.save(commit=False)
             new_short_url.slug = ShortUrl.generate_random_short_slug()
             new_short_url.save()
-            request.session['slug'] = new_short_url.slug
+            request.session["slug"] = new_short_url.slug
         else:
-            return render(request, 'short_urls/index.html', {'formset': ShortUrlForm(), 'not_valid': request.POST['original_url']})
-    
+            request.session["not_valid"] = request.POST["original_url"]
 
-    return render(request, 'short_urls/index.html', {'formset': ShortUrlForm(), "slug": slug, "host": request.get_host()})
+    return HttpResponseRedirect(reverse("short_urls:index"))
 
 
 def redirect_short_url(request, slug):
