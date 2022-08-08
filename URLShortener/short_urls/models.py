c1ff2c188e5977127fde398f7ed73aa61fc05d16
Ourobore <lena.chaprenet@gmail.com>
2022-08-08 10:25:48 +0000

    Reworked bad code, now much more lightweight: no more sketchy validation loops, now using a hashed slug, added 'unique' params on model fields

--- URLShortener/short_urls/models.py
+++ URLShortener/short_urls/models.py
@@ -1,27 +1,41 @@
 import string
-import random
 from django.db import models
 from django.forms import ModelForm
 from django.core.validators import MinLengthValidator, MaxLengthValidator
+from hashids import Hashids
 
 SHORT_URL_LENGTH = 6
 SHORT_URL_CHARSET = string.ascii_letters
+
+hashids = Hashids(
+    alphabet=SHORT_URL_CHARSET,
+    min_length=SHORT_URL_LENGTH,
+)
 
 # Create your models here.
 class ShortUrl(models.Model):
 
     slug = models.CharField(
-        max_length=SHORT_URL_LENGTH,
+        unique=True,
         blank=False,
+        max_length=16,
         validators=[MinLengthValidator(SHORT_URL_LENGTH)],
     )
-    original_url = models.URLField(blank=False, validators=[MaxLengthValidator(2000)])
+    original_url = models.URLField(
+        unique=True,
+        blank=False,
+        validators=[MaxLengthValidator(2000)],
+    )
 
     def __str__(self):
-        return self.slug
+        return f"[{self.slug}] : {self.original_url}"
 
-    def generate_random_short_slug():
-        return "".join(random.choices(SHORT_URL_CHARSET, k=SHORT_URL_LENGTH))
+    def generate_hashed_short_slug():
+        new_id = 0
+        try:
+            new_id = ShortUrl.objects.latest("id").id + 1
+        finally:
+            return hashids.encode(new_id)
 
 
 class ShortUrlForm(ModelForm):
--- URLShortener/short_urls/views.py
+++ URLShortener/short_urls/views.py
@@ -42,47 +42,20 @@ def url_form(request):
     if request.method == "POST":
         formset = ShortUrlForm(request.POST)
         if formset.is_valid():
-            short_url = formset.save(commit=False)
-
             # If original_url is NOT already known, save object and return new slug
-            if not verify_duplicate_original_url(short_url.original_url):
-                # Generate a UNIQUE slug
-                while not short_url.slug or verify_duplicate_slug(short_url.slug):
-                    short_url.slug = ShortUrl.generate_random_short_slug()
-                short_url.save()
-                request.session["slug"] = short_url.slug
+            short_url = formset.save(commit=False)
+            short_url.slug = ShortUrl.generate_hashed_short_slug()
+            short_url.save()
+            request.session["slug"] = short_url.slug
 
-            # Else return existing slug
-            else:
-                request.session["slug"] = ShortUrl.objects.get(
-                    original_url=short_url.original_url
-                ).slug
-
-        # If form is not valid
+        # If the form is not valid OR the URL is already known
         else:
-            request.session["not_valid"] = request.POST["original_url"]
+            try:
+                existing_short_url = ShortUrl.objects.get(
+                    original_url=request.POST["original_url"]
+                )
+                request.session["slug"] = existing_short_url.slug
+            except ShortUrl.DoesNotExist:
+                request.session["not_valid"] = request.POST["original_url"]
 
     return HttpResponseRedirect(reverse("short_urls:index"))
-
-
-#  Logic
-def verify_duplicate_original_url(url):
-    """
-    Checks for duplicate original URL in database
-    """
-    original_url_list = ShortUrl.objects.values_list("original_url", flat=True)
-    if url in list(original_url_list):
-        return True
-    else:
-        return False
-
-
-def verify_duplicate_slug(slug):
-    """
-    Checks for duplicate slug in database
-    """
-    slug_list = ShortUrl.objects.values_list("slug", flat=True)
-    if slug in list(slug_list):
-        return True
-    else:
-        return False
